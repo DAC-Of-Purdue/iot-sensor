cf15d9acd8bba08dac4b95024aa2a5683e6bed93
tame0001 <thirawat.tam@gmail.com>
2023-07-08 16:01:18 -0400

    update demo comment

--- abe3078/src/main.cpp
+++ abe3078/src/main.cpp
@@ -1,12 +1,11 @@
 #include <Arduino.h>
 #include "EspMQTTClient.h"
 #include "DHT.h"
-
-#define DHTPIN 4
-#define DHTTYPE DHT11
+// Define the sensor
+#define DHTPIN 4      // Connection pin
+#define DHTTYPE DHT11 // DHT version
 
 uint32_t chipId = 0;
-
 EspMQTTClient client(
     "DigitalAGClub",  // WiFi SSID at the maker space
     "username",       // WiFi password
@@ -14,44 +13,48 @@ EspMQTTClient client(
     "DAC"             // Client name that uniquely identify your device
 );
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE); // Create a variable for the sensor
 char payload[50];
 
 void onConnectionEstablished()
+// This function activate when connection is established with the broker
 {
-
-  client.subscribe("purdue-dac/command", [](const String &payload)
-                   { Serial.println(payload); });
+  client.subscribe( // Subscribe to a command topic
+      "purdue-dac/command",
+      [](const String &payload)
+      { Serial.println(payload); });
 }
 
 void setup()
 {
   Serial.begin(9600);
-  dht.begin();
+  dht.begin(); // Initialize the sensor
 }
 
 void loop()
 {
   for (int i = 0; i < 17; i = i + 8)
+  // Read chip ID
   {
     chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
   }
-
-  Serial.printf("\nESP32 Chip model = %s Rev %d\n",
-                ESP.getChipModel(), ESP.getChipRevision());
+  // Printing controller detail
+  Serial.printf(
+      "\nESP32 Chip model = %s Rev %d\n",
+      ESP.getChipModel(), ESP.getChipRevision());
   Serial.printf("This chip has %d cores\n", ESP.getChipCores());
   Serial.printf("Chip ID: %X\n", chipId);
-
+  // Read values from the sensor
   delay(10000); // Update 10 seconds
   float h = dht.readHumidity();
   float t = dht.readTemperature(true);
-
+  // Print values to the terminal
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
   Serial.print(F("°F "));
-
+  // Send values to the broker
   client.loop();
   sprintf(payload, "%f", h);
   client.publish("purdue-dac/telemetry/humidity", payload, true);
