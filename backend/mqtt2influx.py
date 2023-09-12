83e0e44c8c6d23a8539dd053b5bed4b6ec9c200a
tbureete <tbureete@purdue.edu>
2023-09-12 16:38:49 -0400

    downgrade library for influx1.6

--- backend/mqtt2influx.py
+++ backend/mqtt2influx.py
@@ -1,44 +1,37 @@
-import os
 import paho.mqtt.subscribe as subscribe
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS, WriteApi
-from dotenv import load_dotenv
 from datetime import datetime
+from influxdb import InfluxDBClient
 
 MQTT_ROOT_TOPIC = "purdue-dac"
-ORG = "Digital Agriculture Club"
 
 
 def on_message_mqtt(_, userdata, message):
-    write_api: WriteApi = userdata["influx_write_api"]
-    bucket = userdata["bucket"]
-    point = Point("ABE3078")
+    influx_client: InfluxDBClient = userdata["influx_client"]
     # extract sensor name from mqtt topic
     sensor_name = message.topic[len(MQTT_ROOT_TOPIC) + 1 :]
-    point = point.tag("sensor_name", sensor_name)
     # extract data in order: temperature, humidity, and timestamp
     data = message.payload.decode("UTF-8").split(":")
-    point = point.field("temperature", float(data[0])).field("humidity", float(data[1]))
     # record timestamp as UTC
     timestamp = datetime.utcfromtimestamp(int(data[2])).isoformat()
-    point = point.time(time=timestamp, write_precision="s")
+    point = [
+        {
+            "measurement": "abe3078",
+            "tags": {"sensor_name": sensor_name},
+            "time": timestamp,
+            "fields": {"temperature": float(data[0]), "humidity": float(data[1])},
+        }
+    ]
     # record data into influxdb
-    write_api.write(bucket=bucket, org=ORG, record=point)
+    influx_client.write_points(point)
     print(f"Record data from {sensor_name} at {timestamp}")
 
+
 if __name__ == "__main__":
-    load_dotenv()
-
-    token = os.environ.get("INFLUXDB_TOKEN")
-    url = "http://localhost:8086"
-
-    influx_client = InfluxDBClient(url=url, token=token, org=ORG)
-    write_api = influx_client.write_api(write_options=SYNCHRONOUS)
-    bucket = "sensor-data"
+    influx_client = InfluxDBClient(host="10.165.77.242", database="dac")
 
     subscribe.callback(
         callback=on_message_mqtt,
         topics=f"{MQTT_ROOT_TOPIC}/#",
         hostname="66.253.158.154",
-        userdata={"influx_write_api": write_api, "bucket": bucket},
+        userdata={"influx_client": influx_client},
     )
