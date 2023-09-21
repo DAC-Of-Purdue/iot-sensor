9e7c467d92da6ddb081827b449da42c13dcd9155
tbureete <tbureete@purdue.edu>
2023-09-20 20:03:44 -0400

    query by period

--- backend/routers/historical_data/crud.py
+++ backend/routers/historical_data/crud.py
@@ -3,15 +3,16 @@ from influxdb import InfluxDBClient
 from influxdb.resultset import ResultSet
 
 
-def query_all_tags(period: str = "5m") -> list[dict]:
+def query_all_tags(period: str | None) -> list[dict]:
     payload = []
+    if period is None:
+        period = "5m"
     try:
         influx_client = InfluxDBClient(database="dac", timeout=10)
         result: ResultSet = influx_client.query(
             f'select * from abe3078 where time > now() - {period} group by "sensor_name";'
         )
         for data in result.items():
-            print(data)
             payload.append(
                 {
                     "sensor_name": data[0][1]["sensor_name"],
--- backend/routers/historical_data/historical_data.py
+++ backend/routers/historical_data/historical_data.py
@@ -9,5 +9,11 @@ router = APIRouter(prefix="/history", tags=["Historical Data"])
     summary="Read historical data from the database",
     response_model=list[schemas.HistoricalDataPayload],
 )
-def read_data():
-    return crud.query_all_tags()
+def read_data(period: str | None = None):
+    """
+    Read historical data. 
+    By default, the API will return data from all devices in past 5 minutes.
+    Optional query parameters:
+    - **period**: query period. For example, "30m" for 30 minutes or "2h" for 2 hours.
+    """
+    return crud.query_all_tags(period=period)
