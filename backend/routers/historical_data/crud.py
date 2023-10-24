722ad7b4fe0ccb683be4b8c0e89fd549da748718
tbureete <tbureete@purdue.edu>
2023-10-24 15:35:14 -0400

    closed Setup retention policy #9

--- backend/routers/historical_data/crud.py
+++ backend/routers/historical_data/crud.py
@@ -3,12 +3,13 @@ from influxdb import InfluxDBClient
 from influxdb.resultset import ResultSet
 
 
-def query_data(period: str | None, sensor_name: str | None = None) -> list[dict]:
+def query_data(period: str | None, sensor_name: str | None = None, raw_data: bool = False) -> list[dict]:
     payload = []
+    database = "raw" if raw_data else "dac"
     if period is None:
-        period = "5m"
+        period = "5m" if raw_data else "12h"
     try:
-        influx_client = InfluxDBClient(database="dac", timeout=10)
+        influx_client = InfluxDBClient(database=database, timeout=10)
         # With specific sensor name
         if sensor_name:
             result: ResultSet = influx_client.query(
