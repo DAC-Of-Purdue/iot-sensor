329ddae7beefd99a68f0b8db7ad081a75ff78485
tbureete <tbureete@purdue.edu>
2023-12-05 16:49:58 -0500

    read soil moisture data

--- groot/src/main.cpp
+++ groot/src/main.cpp
@@ -1,37 +1,47 @@
 #include <Arduino.h>
 #include "EspMQTTClient.h"
 
-#define uS_TO_S_FACTOR 1000000    /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  15         /* Time ESP32 will go to sleep (in seconds) */
+#define LED 2                   // On-board LED
+#define SOIL_SENSOR 36          // Pin for soil sensor
+
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 15        /* Time ESP32 will go to sleep (in seconds) */
 
 EspMQTTClient client(
-  "3031R",
-  "AreUsure",
-  "66.253.158.154",  // MQTT Broker server ip
-  "groot"
-);
+    "3031R",
+    "AreUsure",
+    "66.253.158.154", // MQTT Broker server ip
+    "groot");
+
+char payload[50];
 
 void onConnectionEstablished();
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   client.enableDebuggingMessages();
   client.setWifiReconnectionAttemptDelay(1000);
   esp_sleep_enable_timer_wakeup(60 * TIME_TO_SLEEP * uS_TO_S_FACTOR);
 }
 
-void loop() {
-  while (!client.isConnected()) {
-    Serial.printf("WiFi Connection: %d\n", client.isWifiConnected());
-    Serial.printf("MQTT Connection: %d\n", client.isMqttConnected());
-    delay(5000);
-    client.loop();
+void loop()
+{
+  client.loop();
+
+  if (client.isMqttConnected())
+  {
+    u_int16_t raw_soil_moisture = analogRead(SOIL_SENSOR);
+    Serial.printf("Soil Moisture: %d %\n", raw_soil_moisture);
+    sprintf(payload, "%d", raw_soil_moisture);
+    if (client.publish("purdue-dac/groot", payload))
+    {
+      Serial.println("Going to sleep now");
+      Serial.flush();
+      esp_deep_sleep_start();
+    }
   }
   delay(5000);
-  Serial.println("Going to sleep now");
-  Serial.flush(); 
-  esp_deep_sleep_start();
-  Serial.println("This will never be printed");
 }
 
 void onConnectionEstablished()
