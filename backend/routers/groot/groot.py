ef651b0397fb7bfbb87b5c9247b4db0e5fde82f5
tame0001 <thirawat.tam@gmail.com>
2023-12-08 22:18:47 -0500

    groot api

--- backend/routers/groot/groot.py
+++ backend/routers/groot/groot.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, status
-from fastapi.responses import Response
 from . import schemas
 
 router = APIRouter(prefix="/groot", tags=["Groot (automatic watering) project"])
@@ -15,3 +14,18 @@ def read_command():
     API to tell watering system to activate
     """
     return {"water": True}
+
+
+@router.post(
+    "/command",
+    summary="Check watering is performed",
+    status_code=status.HTTP_201_CREATED
+)
+def feedback(command: schemas.Command):
+    """
+    API to get the feedback from watering system
+    """
+    print(command)
+    print("Done watering ")
+    
+    return 
