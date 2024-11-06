a9268774a05d36d4449ac1a874fa69a5804bb44d
tame0001 <thirawat.tam@gmail.com>
2024-11-06 16:38:24 -0500

    add mosquito.conf

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -3,19 +3,19 @@
 #include "DHT.h"
 #include "time.h"
 // Define the sensor
-#define DHTPIN 4      // Connection pin
+#define DHTPIN 5      // Connection pin
 #define DHTTYPE DHT11 // DHT version
 #define LED 2         // On-board LED
 
-const char* ntpServer = "pool.ntp.org";   // Timestamp server
-char deviceName[20] = "3078-outside";     // Name that uniquely identify your device
+const char *ntpServer = "pool.ntp.org"; // Timestamp server
+char deviceName[20] = "abe3042";        // Name that uniquely identify your device
 
-unsigned long epochTime;                  // Timestamp
+unsigned long epochTime; // Timestamp
 uint32_t chipId = 0;
 EspMQTTClient client(
-    "DigitalAGClub",  // WiFi SSID at the maker space
-    "username",       // WiFi password
-    "66.253.158.154"  // MQTT Broker server IP
+    "DigitalAGClub", // WiFi SSID at the maker space
+    "username",      // WiFi password
+    "broker.emqx.io" // MQTT Broker server IP
 );
 
 DHT dht(DHTPIN, DHTTYPE); // Create a variable for the sensor
@@ -26,8 +26,8 @@ void onConnectionEstablished()
 // This function activate when connection is established with the broker
 {
   Serial.printf("Connection Established\n");
-  digitalWrite(LED, HIGH);                    // Turn on LED
-  client.subscribe(                           // Subscribe to a command topic
+  digitalWrite(LED, HIGH); // Turn on LED
+  client.subscribe(        // Subscribe to a command topic
       "purdue-dac/command",
       [](const String &payload)
       { Serial.println(payload); });
@@ -36,10 +36,10 @@ void onConnectionEstablished()
 void setup()
 {
   Serial.begin(9600);
-  dht.begin();                                  // Initialize the sensor
-  sprintf(topic, "purdue-dac/%s", deviceName);  // Set MQTT Topic
-  pinMode(LED, OUTPUT);                         // Initialize LED
-  digitalWrite(LED, LOW);                       // Set LED to off
+  dht.begin();                                 // Initialize the sensor
+  sprintf(topic, "purdue-dac/%s", deviceName); // Set MQTT Topic
+  pinMode(LED, OUTPUT);                        // Initialize LED
+  digitalWrite(LED, LOW);                      // Set LED to off
   // Printing controller detail
   for (int i = 0; i < 17; i = i + 8)
   // Read chip ID
@@ -53,21 +53,23 @@ void setup()
   Serial.printf("Chip ID: %X\n", chipId);
   client.setMqttClientName(deviceName);
   client.setWifiReconnectionAttemptDelay(5000);
-  configTime(0, 0, ntpServer);                // For timestamp
+  configTime(0, 0, ntpServer); // For timestamp
 }
 
-void blinkLED(uint8_t pin, uint32_t timeDelay) {
+void blinkLED(uint8_t pin, uint32_t timeDelay)
+{
   digitalWrite(pin, !digitalRead(pin));
   delay(timeDelay);
   digitalWrite(pin, !digitalRead(pin));
-
 }
 
-unsigned long getTime() {
+unsigned long getTime()
+{
   time_t now;
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    return(0);
+  if (!getLocalTime(&timeinfo))
+  {
+    return (0);
   }
   time(&now);
   return now;
@@ -81,7 +83,8 @@ void loop()
   epochTime = getTime();
   Serial.printf("Timestamp: %ld\n", epochTime);
   // Check connection
-  if (!client.isConnected() or epochTime == 0) {
+  if (!client.isConnected() or epochTime == 0)
+  {
     Serial.printf("WiFi Connection: %d\n", client.isWifiConnected());
     Serial.printf("MQTT Connection: %d\n", client.isMqttConnected());
     // Need to change to timer
@@ -89,7 +92,8 @@ void loop()
     delay(1000);
   }
   // Read values from the sensor
-  else {
+  else
+  {
     float h = dht.readHumidity();
     float t = dht.readTemperature(true);
     // Print values to the terminal
@@ -102,5 +106,5 @@ void loop()
     Serial.printf("Payload: %s\n", payload);
     blinkLED(LED, 500); // Blink LED to indicate new sending
     delay(10000);       // Update 10 seconds
-  } 
+  }
 }
